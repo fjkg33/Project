2264fa9bf9721ea2f8f1bbb978e3f0493ea173f5
xxseoxx <ssss4482@naver.com>
2024-06-16 20:29:20 +0900

    footer

---
+++ App.js
@@ -0,0 +1,19 @@
+import "./App.css";
+import Footer from "./component/Footer";
+import Footer2 from "./component/Footer2";
+import Body from "./component/Body";
+import Header from "./component/Header";
+
+function App() {
+    return(
+      <>
+        <Header />
+        <Body />
+        <Footer />
+        <Footer2/>
+        
+      </>
+    )
+    
+}
+export default App;
---
+++ Footer.js
@@ -0,0 +1,10 @@
+import "../component/Footer.css";
+
+const Footer =()=>{
+    return(
+        <div className="Footer">
+            <div></div>
+        </div>
+    )
+}
+export default Footer;
